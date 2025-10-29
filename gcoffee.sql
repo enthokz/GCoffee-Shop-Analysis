f4a13ca90f81842a3cc094c94f228d280a9ff721
fahrulrah <fahrul.rah@gmail.com>
2025-10-29 22:47:54 +0700

    update progress. little by little

--- gcoffee.sql
+++ gcoffee.sql
@@ -25,21 +25,33 @@ create view trx_24 as
 -- Total income
  with ab as(
 select extract(month from created_at) as month, sum(final_amount) as total_revenue
-	from trx_month
+	from trx_25
     group by month
     order by month
-    )
-select case when month=4 then 'April'
-		when month=5 then 'Mei'
-        when month=6 then 'June'
+    ),
+    ac as(
+select extract(month from created_at) as month, sum(final_amount) as total_revenue
+	from trx_24
+    group by month
+    order by month),
+    av as(
+select case when az.month=4 then 'April'
+		when az.month=5 then 'Mei'
+        when az.month=6 then 'June'
 	end months,
-    round(total_revenue,2) as total_revenue
-	from ab;
+    round(az.total_revenue,2) as total_revenue_2025,
+    round(ax.total_revenue,2) as total_revenue_2024
+	from ab az
+    join ac ax
+    on az.month=ax.month)
+select months,total_revenue_2024,total_revenue_2025,
+	round(((total_revenue_2025-total_revenue_2024)/total_revenue_2024)*100,2) as growth
+	from av;
 
 -- Whice store was underperformed
 with aa as(
 select store_id, round(sum(final_amount),2) as total_value
-	from trx_month
+	from trx_25
     group by store_id)
 select az.store_id, store_name,state, total_value
 	from aa az
@@ -50,7 +62,7 @@ select az.store_id, store_name,state, total_value
 -- Number of customer from each site
 with aa as(
 select store_id, count(distinct transaction_id)  as number_cust
-	from trx_month
+	from trx_25
     group by store_id)
 select rank() over(order by number_cust desc) as ranking, az.store_id, store_name, state, number_cust
 	from aa az
@@ -61,7 +73,7 @@ select rank() over(order by number_cust desc) as ranking, az.store_id, store_nam
 -- Is there lack of cust based on region sales?
 with aa as(
 select store_id, count(distinct transaction_id)  as number_cust
-	from trx_month
+	from trx_25
     group by store_id)
 select state, round(avg(number_cust),2) as avg_cust
 	from aa az
@@ -70,5 +82,47 @@ select state, round(avg(number_cust),2) as avg_cust
     group by state
     order by avg_cust;
 
+-- What best and underperform product 
+with aa as(
+select az.transaction_id,ax.item_id, quantity, ac.item_name 
+	from trx_25 az
+    join items_2504 ax
+    on az.transaction_id=ax.transaction_id
+    join menu_items ac
+    on ax.item_id=ac.item_id
+    where store_id=6)
+select item_id, item_name, sum(quantity) as qty_ordered
+	from aa
+    group by item_id,item_name;
+    
+-- Purchase by Member
+with aa as(
+select extract(month from created_at) as month, count(*) as purchase_member
+	from trx_25
+    where user_id is not null
+    group by month
+    order by month),
+	aq as(
+select extract(month from created_at) as month, count(*) as purchase_nonmember
+	from trx_25
+    where user_id is null
+    group by month
+    order by month)
+select az.month, purchase_member, purchase_nonmember
+	from aa az
+    join aq ax
+    on az.month=ax.month;
+    
+-- Any voucher for member used?
+select distinct az.voucher_id, voucher_code, discount_type,
+	count(ax.voucher_id) as ordered_use_voucher
+	from trx_25 ax
+    join vouchers az
+    on az.voucher_id=ax.voucher_id
+    group by voucher_id, voucher_code, discount_type;
+
+
+select *
+	FROM vouchers;
 select store_name, postal_code, lag(postal_code) over(order by store_name) as selisih
 	from stores;
